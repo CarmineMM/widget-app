eec38cafc45a37df14b2ad5b488739f1b1afc426
Carmine Maggio <carminemaggiom@gmail.com>
2025-04-17 16:09:23 -0400

    feat: new provider

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
-  final int count = 0;
-
+class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
 
   static const String route = 'counter';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final textStyle = Theme.of(context).textTheme.displayMedium;
+    final int value = ref.watch(counterProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Counter Screen')),
-      body: Center(child: Text('Valor: $count', style: textStyle)),
+      body: Center(child: Text('Valor: $value ', style: textStyle)),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
         child: const Icon(Icons.add),
+        onPressed: () {
+          // ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
       ),
     );
   }
